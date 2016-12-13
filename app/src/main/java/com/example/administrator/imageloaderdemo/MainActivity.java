5a0808e7b82df70c0d96e9eaf07a9f006d6a99a7
Sijinyuan <2585675654@qq.com>
2016-12-14 07:21:57 +0800

    Initial commit2016/12/14 ImageLoader缓存机制

---
+++ app/src/main/java/com/example/administrator/imageloaderdemo/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.administrator.imageloaderdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewTreeObserver;
+
+public class MainActivity extends AppCompatActivity {
+    private int mImageThumbSize;
+    private int mImageThumbSpacing;
+    private RecyclerView mPhotoWall;
+    private PhotoWallAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageThumbSize = getResources().getDimensionPixelSize(
+                R.dimen.image_thumbnail_size);
+        mImageThumbSpacing = getResources().getDimensionPixelSize(
+                R.dimen.image_thumbnail_spacing);
+        mPhotoWall=(RecyclerView)findViewById(R.id.recyclerview);
+        GridLayoutManager layoutManager = new GridLayoutManager(this,3);
+        //设置布局管理器
+        mPhotoWall.setLayoutManager(layoutManager);
+        //设置为垂直布局，这也是默认的
+        layoutManager.setOrientation(OrientationHelper.VERTICAL);
+        mAdapter = new PhotoWallAdapter(this, 0, Images.imageThumbUrls,
+                mPhotoWall);
+        mPhotoWall.setAdapter(mAdapter);
+        mPhotoWall.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+
+                    @Override
+                    public void onGlobalLayout() {
+                        final int numColumns = (int) Math.floor(mPhotoWall
+                                .getWidth()
+                                / (mImageThumbSize + mImageThumbSpacing));
+                        if (numColumns > 0) {
+                            int columnWidth = (mPhotoWall.getWidth() / numColumns)
+                                    - mImageThumbSpacing;
+                            mAdapter.setItemHeight(columnWidth);
+                            mPhotoWall.getViewTreeObserver()
+                                    .removeGlobalOnLayoutListener(this);
+                        }
+                    }
+                });
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAdapter.fluchCache();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 退出程序时结束所有的下载任务
+        mAdapter.cancelAllTasks();
+    }
+}
